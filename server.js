74396d5f590b20e2075f1bbc047187b70a73cbdc
chris-w-jarvis <cjarvis@email.arizona.edu>
2018-03-27 18:44:04 -0700

    fix npm start

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('views'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect('/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${ port }`);
+});
